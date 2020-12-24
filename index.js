350bf1c6bba282eb517801a46778df5dd2ced878
dperolio <dustin.perolio@outlook.com>
2020-12-24 01:01:46 -0500

    ESM syntax update and code cleanup using protocol links

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-const { join } = require('path');
+import { readdirSync } from 'fs';
+import { join } from 'path';
 
-const { array: { getRandomArrayItem }, file: { getObjectURL } } = require('@vizality/util');
-const { Plugin } = require('@vizality/entities');
+import { getRandomArrayItem } from '@vizality/util/array';
+import { Plugin } from '@vizality/core';
 
-module.exports = class HeyZere extends Plugin {
-  constructor () {
-    super();
-    this.URLs = [];
-    this.observer = null;
-  }
-
-  onStart () {
+export default class HeyZere extends Plugin {
+  async onStart () {
     vizality.api.commands.registerCommand({
       command: 'heyzere',
       description: 'Replaces every image with a random image of Zerebos.',
       usage: '{c}',
-      executor: this._convertToBlobURL.bind(this)
+      executor: this.heyZere.bind(this)
     });
+
+    this.zeres = readdirSync(join(__dirname, 'assets', 'zerebos'), { withFileTypes: true })
+      .filter(item => !item.isDirectory())
+      .map(item => item.name);
+
+    this.zeres = this.zeres.map(z => `vz-plugin://${this.addonId}/assets/zerebos/${z}`);
   }
 
   async onStop () {
     clearInterval(this.interval);
     vizality.api.commands.unregisterCommand('heyzere');
-    await this.URLs.forEach(url => URL.revokeObjectURL(url));
-    this.URLs = [];
-  }
-
-  async _convertToBlobURL () {
-    this.URLs = await getObjectURL(join(__dirname, 'assets', 'zerebos'));
-    this.heyZere();
   }
 
   heyZere () {
     this.interval = setInterval(() => {
       document.querySelectorAll('[style*="background-image"]')
         .forEach(({ style }) => {
-          if (!this.URLs.filter(url => style.backgroundImage.includes(url)).length) {
-            style.backgroundImage = `url("${getRandomArrayItem(this.URLs)}")`;
+          if (!this.zeres.filter(url => style.backgroundImage.includes(url)).length) {
+            style.backgroundImage = `url(${getRandomArrayItem(this.zeres)})`;
           }
         });
 
       document.querySelectorAll('img')
         .forEach(image => {
-          if (!this.URLs.includes(image.src)) {
-            image.src = getRandomArrayItem(this.URLs);
+          if (!this.zeres.includes(image.src)) {
+            image.src = getRandomArrayItem(this.zeres);
           }
         });
     }, 50);
   }
-};
+}
